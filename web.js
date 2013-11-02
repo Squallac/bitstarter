17a17dac9f1709ef11c5d053a3720b80d6849b8e
Squallac <squall_ff@hotmail.com>
2013-11-02 17:24:08 +0000

    another code from web.js

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
+var fs = require ('fs');
 var app = express();
 app.use(express.logger());
 
 	  
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-//var indice = fs.readFileSync('index.html');
-var buffer = new Buffer();
- response.send(buffer.toString('utc-8',fs.readFileSync("index.html")));
+
+var indice = fs.readFileSync("index.html",'utf-8');
+//var buffer = new Buffer();
+ response.send(indice);
 });
 
 var port = process.env.PORT || 5000;
