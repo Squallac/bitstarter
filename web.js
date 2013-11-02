58f232d642ad08c178be5d82fabc56f90d01f1db
Squallac <squall_ff@hotmail.com>
2013-11-02 06:53:00 +0000

    Added modified web.js

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+
+var index=fs.readFileSync('index.html');
+var rectificado=index.toString('utf8');			  
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
@@ -10,3 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
