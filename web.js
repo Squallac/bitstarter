e325a29d5de86aaa494e2e69811e063638edd8de
Squallac <squall_ff@hotmail.com>
2013-11-02 07:00:06 +0000

    Corrected web.js

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
-
-var index=fs.readFileSync('index.html');
-var rectificado=index.toString('utf8');			  
-
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var index=fs.readFileSync('index.html');
+var rectificado=index.toString('utf8');			  
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(rectificado);
 });
 
 var port = process.env.PORT || 5000;
